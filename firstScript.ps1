89686c3580d6c12861f09362a1371b07367e0d48
livevivevil <livevivevil@gmail.com>
2024-09-20 09:11:13 -0400

    Added ChildItem command

--- firstScript.ps1
+++ firstScript.ps1
@@ -6,3 +6,8 @@ $c=$a + $b
 $d =$a - $b
 Write-host $c
 Write-host $d
+
+Get-ChildItem -path C:\ -force
+
+Get-ChildItem -path $env:ProgramFiles -Recurse *.exe | 
+Where-Object -FilterScript {($_.LastWriteTime -gt (2020-01-01)) -and ($_.Length -ge 10mb) -and ($_.Length -le 200mb) }
